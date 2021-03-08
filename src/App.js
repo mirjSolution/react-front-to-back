f621b244676be91332652377178daa609e4c201a
mirjSolution <ricojohndatoon23@gmail.com>
2021-03-08 18:54:49 -0500

    Functional Components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// Name export
+// import {Greet} from './components/functional/greet';
+import Greet from './components/functional/greet';
 
 class App extends React.Component { 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>Hello World</h1>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {/* Sample functinal components */}
+        <Greet />
       </div>
     );
   }
---
+++ src/components/functional/greet.js
@@ -0,0 +1,15 @@
+// Sample functional components
+import React from 'react'
+
+// Traditional
+// function Greet() {
+//     return <h1>Hello Bonbon</h1>
+// }
+
+// ES6
+const Greet = () => <h1>Hello Bonbon</h1>
+
+export default Greet;
+
+// Another method of exporting using name export
+// export const Greet = () => <h1>Hello Bonbon</h1>
