786a7e0803cae6d74d5287c34e691286816ad53c
mirjSolution <ricojohndatoon23@gmail.com>
2021-03-11 09:57:54 -0500

    Index as a key on list

--- src/components/listRendering/NameList.js
+++ src/components/listRendering/NameList.js
@@ -2,32 +2,32 @@ import Person from './Person';
 
 function NameList() {
   // Using Array
-  //   const names = ['Bruce', 'Clark', 'Diana'];
-  //   const nameList = names.map((name) => <h2>{name}</h2>);
+  const names = ['Bruce', 'Clark', 'Diana'];
+  const nameList = names.map((name) => <h2>{name}</h2>);
   //   Using Object
-  const persons = [
-    {
-      id: 1,
-      name: 'Bruce',
-      age: 30,
-      skill: 'React',
-    },
-    {
-      id: 2,
-      name: 'Clark',
-      age: 25,
-      skill: 'Angular',
-    },
-    {
-      id: 3,
-      name: 'Diana',
-      age: 28,
-      skill: 'Vue',
-    },
-  ];
-  const personList = persons.map((person) => (
-    <Person key={person.id} person={person} />
-  ));
+  //   const persons = [
+  //     {
+  //       id: 1,
+  //       name: 'Bruce',
+  //       age: 30,
+  //       skill: 'React',
+  //     },
+  //     {
+  //       id: 2,
+  //       name: 'Clark',
+  //       age: 25,
+  //       skill: 'Angular',
+  //     },
+  //     {
+  //       id: 3,
+  //       name: 'Diana',
+  //       age: 28,
+  //       skill: 'Vue',
+  //     },
+  //   ];
+  //   const personList = persons.map((person) => (
+  //     <Person key={person.id} person={person} />
+  //   ));
   return (
     //   Approach 1 using array
     // <div>
@@ -36,9 +36,10 @@ function NameList() {
     //   ))}
     // </div>
     // Approach 2 using array
-    // <div>{nameList}</div>
+    <div>{nameList}</div>
     // Approach 3 using object passing the props in components
-    <div>{personList}</div>
+    // Approach 3 using object
+    // <div>{personList}</div>
   );
 }
 
